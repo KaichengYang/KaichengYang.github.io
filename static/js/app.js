2282480472dcf92c36d2f45c919f3cfa5af00af4
yangkc <yangkc@iu.edu>
2020-09-06 19:00:31 -0400

    add categories

--- static/js/app.js
+++ static/js/app.js
@@ -3,7 +3,15 @@ var app = new Vue({
     data: {
         message: 'Hello Vue!',
         json: null,
-        refs: null
+        refs: null,
+        btns: {
+            all: "btn-clicked",
+            tool: "",
+            open: "",
+            pub: "",
+            work: "",
+            preprint: ""
+        }
     },
     created: function () {
         var _this = this;
@@ -11,8 +19,70 @@ var app = new Vue({
             _this.json = json;
         });
         $.getJSON('/static/files/refs.json', function (json) {
+            _this._disable_highlight(json);
+            _this._show_all_pub(json);
+
+            json.forEach(project => {
+                project.papers.forEach(paper => paper.show=true)
+            });
+
             _this.refs = json;
         });
+
+    },
+    methods: {
+        reset_btns : function () {
+            Object.keys(this.btns).forEach(key => {
+                this.btns[key] = "";
+            });
+        },
+        _show_all_pub: function (array) {
+            array.forEach(project => {
+                project.papers.forEach(paper => paper.show=true)
+            });
+        },
+        pubfilter_default: function () {
+            this._show_all_pub(this.refs);
+            this.reset_btns();
+            this.btns.all = "btn-clicked"
+        },
+        pubfilter_with_link_to: function (target, btn){
+            this.refs.forEach(project => {
+                project.papers.forEach(paper => paper.show=this.has_link_to(paper, target))
+            });
+            this.reset_btns();
+            this.btns[btn] = "btn-clicked"
+        },
+        pubfilter_without_link_to: function (target, btn){
+            this.refs.forEach(project => {
+                project.papers.forEach(paper => paper.show=!this.has_link_to(paper, target))
+            });
+            this.reset_btns();
+            this.btns[btn] = "btn-clicked"
+        },
+        has_link_to: function (paper_obj, target) {
+            for (i=0; i<paper_obj.links.length; i++){
+                if (target.indexOf(paper_obj.links[i].name) > -1) {
+                    return true;
+                }
+            }
+            return false;
+        },
+
+        highlight_pub: function (index) {
+            this.disable_highlight();
+            this.refs[2].papers[index].highlight = true;
+        },
+        disable_highlight: function () {
+            this._disable_highlight(this.refs)
+        },
+        _disable_highlight: function (array) {
+            array.forEach(project => {
+                project.papers.forEach(paper =>{
+                    paper.highlight = false;
+                })
+            });
+        }
     }
 })
 
