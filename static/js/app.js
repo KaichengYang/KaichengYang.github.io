2ec1a65ce35d594462bdf3a17b752739a9cf9953
yangkc <yangkc@iu.edu>
2020-09-09 15:30:22 -0400

    adjust the icons

--- static/js/app.js
+++ static/js/app.js
@@ -11,6 +11,13 @@ var app = new Vue({
             pub: "",
             work: "",
             preprint: ""
+        },
+        pub_icon_map: {
+            arXiv: "ai ai-arxiv ai-1x",
+            DOI: "far fa-file-pdf",
+            SocArXiv: "far fa-file-pdf",
+            GitHub: "fab fa-github-square",
+            Dataset: "fas fa-database"
         }
     },
     created: function () {
@@ -31,7 +38,14 @@ var app = new Vue({
 
     },
     methods: {
-        reset_btns : function () {
+        get_pub_icon: function (c) {
+            var icon = this.pub_icon_map[c];
+            if (icon === undefined) {
+                icon = "fas fa-external-link-alt";
+            }
+            return icon;
+        },
+        reset_btns: function () {
             Object.keys(this.btns).forEach(key => {
                 this.btns[key] = "";
             });
