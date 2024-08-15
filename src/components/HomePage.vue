ac3f0fd78d5ef63a8ad777f4496302eff5c87925
yang3kc <yang3kc@gmail.com>
2024-08-15 00:14:51 -0400

    fix: home wording

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -16,7 +16,7 @@
     <!-- Pubs -->
     <h2 class="text-4xl font-medium mb-2 flex justify-center" id="pubs">Selected publications</h2>
     <p class="prose mb-4 text-center">
-      See the full publication list <router-link to="/pubs" class="link">here</router-link> or from my <a href="https://scholar.google.com/citations?hl=en&user=tqb96X8AAAAJ&view_op=list_works&sortby=pubdate" target="_blank" class="link"> <font-awesome-icon :icon="['fab', 'google-scholar']" class="text-medium" /> Google Scholar</a> page
+      See the longer list <router-link to="/pubs" class="link">here</router-link> or full list from my <a href="https://scholar.google.com/citations?hl=en&user=tqb96X8AAAAJ&view_op=list_works&sortby=pubdate" target="_blank" class="link"> <font-awesome-icon :icon="['fab', 'google-scholar']" class="text-medium" /> Google Scholar</a> page
     </p>
     <PubsList :is_home="true" />
 
