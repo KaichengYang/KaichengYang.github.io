a850ee63c56dcf724850c95652aa786c653f3c72
yang3kc <yang3kc@gmail.com>
2024-08-19 18:19:16 -0400

    fix: rewrite the code in composite api

--- src/components/media/MediaList.vue
+++ src/components/media/MediaList.vue
@@ -1,134 +1,92 @@
-<template>
-  <hr class="col-span-full my-3">
-  <div v-for="media in media_to_show" :key="media.title" class="grid grid-cols-1 md:grid-cols-7 md:gap-x-1">
-    <!-- outlet logo small -->
-    <div class="col-span-full mb-2 flex justify-center md:hidden">
-      <img :src="'/logos/' + media.outlet + '.png'" alt="logo" style="height: 30px;">
-    </div>
-    <!-- date normal -->
-    <div class="font-mono col-span-1 text-center max-md:hidden">
-      <span class="badge mt-1 text-white w-28" style="background-color: #867d78;">{{ media.date }}</span>
-    </div>
-    <!-- title -->
-    <div class="prose col-span-full md:col-span-4 text-center md:text-left">
-      <a :href="media.url" target="_blank" class="link">{{ media.title }} <font-awesome-icon :icon="['fas', 'link']" /></a>
-    </div>
-    <!-- outlet logo normal -->
-    <div class="max-md:hidden md:col-span-2 mr-2">
-      <img :src="'/logos/' + media.outlet + '.png'" class="float-right" alt="logo" style="height: 30px;">
-    </div>
-    <!-- date small -->
-    <div class="font-mono col-span-full text-center md:hidden">
-      <span class="badge mt-1 text-white w-28" style="background-color: #867d78;">{{ media.date }}</span>
-    </div>
-    <!-- type -->
-    <div class="font-mono col-span-full md:col-span-1 text-center">
-      <span v-if="media.type == 'interview'" class="badge mt-1 text-white w-28" style="background-color: #006298;">Interview</span>
-      <span v-if="media.type == 'quotation'" class="badge mt-1 text-white w-28" style="background-color: #006298;">Quotation</span>
-      <span v-if="media.type == 'coverage' && media.ref" class="badge mt-1 text-white w-28" style="background-color: #ac4142;">Featuring</span>
-    </div>
-    <!-- ref -->
-    <div class="prose col-span-full md:col-span-4 text-center md:text-left">
-      <template v-if="media.type == 'coverage' && media.ref">
-        <template v-for="(ref, index) in media.ref" :key="index">
-          <template v-if="index > 0"> and </template>
-          <a :href="ref.link.url" target="_blank" class="text-gray-400 font-normal hover:decoration-2"> {{ ref.title }}</a>
-        </template>
-      </template>
-    </div>
-    <!-- media outlet name normal -->
-    <div class="md:col-span-2 prose max-md:hidden mr-2">
-      <p class="text-right">{{ media.outlet }}</p>
-    </div>
-    <hr class="col-span-full my-3">
-  </div>
-  <BackForth :is_home="is_home" target="/media"/>
-</template>
-
-<script>
-import { defineComponent } from 'vue';
+<script setup>
+import { ref, computed, onMounted } from 'vue';
 import BackForth from '@/components/nav/BackForth.vue';
+import MediaBlock from '@/components/media/MediaBlock.vue';
 
-export default defineComponent({
-  name: 'MediaList',
-  components: {
-    BackForth
+// props
+const props = defineProps({
+  is_home: {
+    type: Boolean,
+    default: true
   },
-  props: {
-    is_home: {
-      type: Boolean,
-      default: true
-    },
-    view: {
-      type: String,
-      default: "year"
-    }
-  },
-  data() {
-    return {
-      media_list: [],
-      tools_list: [],
-      pubs_list: [],
-      media_list_with_attr: [],
-      color_map: {
-        "date": {"color": "#867d78"},
-        "outlet": {"color": "#ac4142"}
-      }
-    };
-  },
-  created() {
-    Promise.all([
-      fetch('/files/media.json').then(response => response.json()),
-      fetch('/files/tools.json').then(response => response.json()),
-      fetch('/files/refs.json').then(response => response.json())
-    ]).then(([mediaData, toolsData, refsData]) => {
-      this.media_list = mediaData;
-      this.tools_list = toolsData;
-      this.pubs_list = refsData;
-      // Do something else here after all fetches are complete
-      this.media_list_with_attr = this.assign_projects_to_media(this.pubs_list, this.tools_list, this.media_list);
-    });
-  },
-  computed: {
-    media_to_show() {
-      let temp_media_list = this.media_list.filter(media => media.toshow);
-      if (this.is_home) {
-        return temp_media_list.filter(media => media.highlight).slice(0, 5);
-      } else {
-        return temp_media_list;
-      }
-    }
-  },
-  methods: {
-    assign_projects_to_media(refs_json, tools_json, media_json) {
-      media_json.forEach(function (media) {
-        let ref_list = [];
-        tools_json.forEach(function (tool) {
-          if (media.project_id && media.project_id.indexOf(tool.id) >= 0) {
-            ref_list.push({
-              "title": tool.title,
-              "link": tool.links[0]
-            });
-          }
-        });
-        refs_json.forEach(function (ref) {
-          if (media.project_id && media.project_id.indexOf(ref.id) >= 0) {
-            ref_list.push({
-              "title": ref.title,
-              "link": ref.links[0]
-            });
-          }
-        });
-
-        if (ref_list.length > 0) {
-          media["ref"] = ref_list;
-        }
-      });
-      return media_json;
-    }
+  view: {
+    type: String,
+    default: "year"
   }
 });
+
+// data
+const media_list = ref([]);
+const tools_list = ref([]);
+const pubs_list = ref([]);
+const media_list_with_attr = ref([]);
+
+// computed
+const media_to_show = computed(() => {
+  let temp_media_list = media_list.value.filter(media => media.toshow);
+  if (props.is_home) {
+    return temp_media_list.filter(media => media.highlight).slice(0, 5);
+  } else {
+    return temp_media_list;
+  }
+});
+
+// methods
+const fetchData = async () => {
+  const [mediaData, toolsData, refsData, pubIndex] = await Promise.all([
+    fetch('/files/media.json').then(response => response.json()),
+    fetch('/files/tools.json').then(response => response.json()),
+    fetch('/files/refs.json').then(response => response.json()),
+    fetch('/files/pubs/pubs_index.json').then(response => response.json())
+  ]);
+  media_list.value = mediaData;
+  tools_list.value = toolsData;
+  pubs_list.value = refsData;
+  pubIndex.forEach(
+    pub_file_name => {
+      fetch(`/files/pubs/${pub_file_name}`)
+        .then(response => response)
+        .then(pub => {
+          pubs_list.push(pub);
+        });
+    }
+  );
+  media_list_with_attr.value = assign_projects_to_media(pubs_list.value, tools_list.value, media_list.value);
+};
+
+const assign_projects_to_media = (refs_json, tools_json, media_json) => {
+  media_json.forEach(media => {
+    let ref_list = [];
+    tools_json.forEach(tool => {
+      if (media.project_id && media.project_id.indexOf(tool.id) >= 0) {
+        ref_list.push({
+          "title": tool.title,
+          "link": tool.links[0]
+        });
+      }
+    });
+    refs_json.forEach(ref => {
+      if (media.project_id && media.project_id.indexOf(ref.id) >= 0) {
+        ref_list.push({
+          "title": ref.title,
+          "link": ref.links[0]
+        });
+      }
+    });
+
+    if (ref_list.length > 0) {
+      media["ref"] = ref_list;
+    }
+  });
+  return media_json;
+};
+
+onMounted(fetchData);
+
 </script>
 
-<style scoped>
-</style>
+<template>
+  <hr class="col-span-full my-3">
+  <MediaBlock v-for="media in media_to_show" :key="media.title" :media="media" />
+  <BackForth :is_home="is_home" target="/media"/>
+</template>
