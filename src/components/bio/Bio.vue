6427747804ebaa3853efc905bdbc2f62a90c12d8
yang3kc <yang3kc@gmail.com>
2024-08-25 21:51:19 -0400

    fix: better formatting

--- src/components/bio/Bio.vue
+++ src/components/bio/Bio.vue
@@ -15,45 +15,43 @@
       <!-- contact -->
       <div id="contact_container" class="mt-1">
         <a href="mailto:yang3kc@gmail.com" class="text-primary mr-1">
-            <font-awesome-icon :icon="['fas', 'envelope']" class="text-2xl" />
+          <font-awesome-icon :icon="['fas', 'envelope']" class="text-2xl" />
         </a>
         <a href="https://scholar.google.com/citations?hl=en&user=tqb96X8AAAAJ&view_op=list_works&sortby=pubdate" class="text-primary mr-1" target="_blank">
-            <font-awesome-icon :icon="['fab', 'google-scholar']" class="text-2xl" />
+          <font-awesome-icon :icon="['fab', 'google-scholar']" class="text-2xl" />
         </a>
         <a href="https://www.researchgate.net/profile/Kai-Cheng-Yang-3" class="text-primary mr-1" target="_blank">
-            <font-awesome-icon :icon="['fab', 'researchgate']" class="text-2xl" />
+          <font-awesome-icon :icon="['fab', 'researchgate']" class="text-2xl" />
         </a>
         <a href="https://www.linkedin.com/in/kaicheng-yang-43477213b/" class="text-primary mr-1" target="_blank">
-            <font-awesome-icon :icon="['fab', 'linkedin']" class="text-2xl" />
+          <font-awesome-icon :icon="['fab', 'linkedin']" class="text-2xl" />
         </a>
         <a href="https://twitter.com/yang3kc" class="text-primary mr-1" target="_blank">
-            <font-awesome-icon :icon="['fab', 'twitter']" class="text-2xl" />
+          <font-awesome-icon :icon="['fab', 'twitter']" class="text-2xl" />
         </a>
         <a href="https://yang3kc.substack.com" class="text-primary mr-1" target="_blank">
-            <font-awesome-icon :icon="['fas', 'bookmark']" class="text-2xl" />
+          <font-awesome-icon :icon="['fas', 'bookmark']" class="text-2xl" />
         </a>
       </div>
     </div>
-      <!-- bio -->
+    <!-- bio -->
     <div id="bio" class="md:col-span-2 mt-2 mx-4 prose">
       <p>
-          Hi! I'm Kai-Cheng Yang (pronounced KY-cheng YAHNG). You can also call me Kevin.
-          I'm a postdoctoral researcher at Northeastern University working with <a class="link" href="https://lazerlab.net" target="_blnak">David Lazer</a>.
-          I obtained my Ph.D. in Informatics from <a class="link" href="https://luddy.indiana.edu/" target="_blank">
-              Luddy School of Informatics, Computing, and Engineering</a>
-          in Indiana University Bloomington, advised by <a class="link" href="http://cnets.indiana.edu/fil/" target="_blank">Filippo Menczer</a>.
+        Hi! I'm Kai-Cheng Yang (pronounced KY-cheng YAHNG). You can also call me Kevin.
+        I'm a postdoctoral researcher at Northeastern University working with <a class="link" href="https://lazerlab.net" target="_blnak">David Lazer</a>.
+        I obtained my Ph.D. in Informatics from <a class="link" href="https://luddy.indiana.edu/" target="_blank">
+            Luddy School of Informatics, Computing, and Engineering</a>
+        in Indiana University Bloomington, advised by <a class="link" href="http://cnets.indiana.edu/fil/" target="_blank">Filippo Menczer</a>.
       </p>
       <p>
-          My research focuses on creating safe, fair, and trustworthy online information platforms by identifying how malicious actors and flawed systems distort information flow and developing effective countermeasures.
-          My work spans social bots, misinformation, and algorithmic biases.
-          Currently, I'm exploring how generative AI is being misused in these contexts and how to harness this technology to protect against these threats.
+        My research focuses on creating safe, fair, and trustworthy online information platforms by identifying how malicious actors and flawed systems distort information flow and developing effective countermeasures.
+        My work spans social bots, misinformation, and algorithmic biases.
+        Currently, I'm exploring how generative AI is being misused in these contexts and how to harness this technology to protect against these threats.
       </p>
       <p>
-          You can check out my
-          <!-- <a class="text-primary" href="#project">research projects</a> and -->
-          <a class="link" href="#pubs">publications</a> to learn more.
-          These works have been <a class="link" href="#media">covered</a> by various media outlets.
-          You might also find some of my <a class="link" href="#tools">tools</a> useful.
+        You can check out my <a class="link" href="#pubs">publications</a> to learn more.
+        These works have been <a class="link" href="#media">covered</a> by various media outlets.
+        You might also find some of my <a class="link" href="#tools">tools</a> useful.
       </p>
     </div>
   </div>
