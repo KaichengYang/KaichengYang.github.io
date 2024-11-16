a3614e3b093f9b42ab9cf88ea234362050c5ac5f
yang3kc <yang3kc@gmail.com>
2024-11-16 09:14:57 -0500

    import bluesky icon

--- src/main.js
+++ src/main.js
@@ -11,10 +11,10 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 /* import specific icons */
 import { faEnvelope, faFile, faBookmark, faCircleArrowLeft, faCircleArrowRight, faLink, faDatabase, faUser, faCircleUser, faDownload } from '@fortawesome/free-solid-svg-icons'
-import { faFilePdf as faFilePdfRegular, faUser as faUserRegular, faCircleUser as faCircleUserRegular, faEnvelope as faEnvelopeRegular, faCopy as faCopyRegular, faCircleXmark as faCircleXmarkRegular} from '@fortawesome/free-regular-svg-icons'
-import { faLinkedin, faTwitter, faGoogleScholar, faResearchgate, faGithub, faPython, faRProject } from '@fortawesome/free-brands-svg-icons'
+import { faFilePdf as faFilePdfRegular, faUser as faUserRegular, faCircleUser as faCircleUserRegular, faEnvelope as faEnvelopeRegular, faCopy as faCopyRegular, faCircleXmark as faCircleXmarkRegular } from '@fortawesome/free-regular-svg-icons'
+import { faLinkedin, faTwitter, faGoogleScholar, faResearchgate, faGithub, faPython, faRProject, faBluesky } from '@fortawesome/free-brands-svg-icons'
 /* add icons to the library */
-library.add(faEnvelope, faFile, faBookmark, faLinkedin, faTwitter, faGoogleScholar, faResearchgate, faCircleArrowLeft, faCircleArrowRight, faLink, faFilePdfRegular, faDatabase, faGithub, faUser, faCircleUser, faUserRegular, faCircleUserRegular, faEnvelopeRegular, faCopyRegular, faCircleXmarkRegular, faDownload, faPython, faRProject)
+library.add(faEnvelope, faFile, faBookmark, faLinkedin, faTwitter, faGoogleScholar, faResearchgate, faCircleArrowLeft, faCircleArrowRight, faLink, faFilePdfRegular, faDatabase, faGithub, faUser, faCircleUser, faUserRegular, faCircleUserRegular, faEnvelopeRegular, faCopyRegular, faCircleXmarkRegular, faDownload, faPython, faRProject, faBluesky)
 
 async function initApp() {
   const pub_list = ref([]);
