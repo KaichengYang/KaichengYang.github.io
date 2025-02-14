76f59f56079f079dbbae9b29e5fb6c2b35c598f5
yang3kc <yang3kc@gmail.com>
2025-02-14 17:46:04 -0500

    update bio

--- src/components/bio/Bio.vue
+++ src/components/bio/Bio.vue
@@ -42,47 +42,66 @@ const contact_links = ref([
 </script>
 
 <template>
-  <div class="grid grid-cols-1 md:grid-cols-3 gap-4 mt-4">
-  <!-- avatar -->
-    <div id="avatar_container" class="col-span-1 flex flex-col items-center justify-center">
-      <!-- photo -->
-      <div id="avatar" class="mx-auto">
-        <img src="/photos/avatar_01.png" alt="Avatar" class="avatar rounded-full">
+  <div>
+    <div class="grid grid-cols-1 md:grid-cols-3 gap-4 mt-4">
+    <!-- avatar -->
+      <div id="avatar_container" class="col-span-1 flex flex-col items-center justify-center">
+        <!-- photo -->
+        <div id="avatar" class="mx-auto">
+          <img src="/photos/avatar_01.png" alt="Avatar" class="avatar rounded-full">
+        </div>
+        <!-- info -->
+        <div id="avatar_info" class="text-center">
+          <h2 class="text-2xl font-medium">Kai-Cheng Yang, Ph.D.</h2>
+          <p id="title" class="text-xl text-gray-500">Postdoctoral researcher</p>
+          <p id="title" class="text-xl text-gray-500">Northeastern University</p>
+        </div>
+        <!-- contact -->
+        <div id="contact_container" class="mt-1">
+          <template v-for="link in contact_links" :key="link.name">
+            <a :href="link.link" class="text-primary mr-1" target="_blank">
+              <font-awesome-icon :icon="[link.icon.prefix, link.icon.icon]" class="text-2xl" />
+            </a>
+          </template>
+        </div>
       </div>
-      <!-- info -->
-      <div id="avatar_info" class="text-center">
-        <h2 class="text-2xl font-medium">Kai-Cheng Yang, Ph.D.</h2>
-        <p id="title" class="text-xl text-gray-500">Postdoctoral researcher</p>
-        <p id="title" class="text-xl text-gray-500">Northeastern University</p>
-      </div>
-      <!-- contact -->
-      <div id="contact_container" class="mt-1">
-        <template v-for="link in contact_links" :key="link.name">
-          <a :href="link.link" class="text-primary mr-1" target="_blank">
-            <font-awesome-icon :icon="[link.icon.prefix, link.icon.icon]" class="text-2xl" />
-          </a>
-        </template>
+      <!-- bio -->
+      <div id="bio" class="md:col-span-2 mt-2 mx-4 prose">
+        <p>
+          Hi! I'm Kai-Cheng Yang (pronounced KY-cheng YAHNG). You can also call me Kevin.
+          I'm a postdoctoral researcher at Northeastern University working with <a class="link" href="https://lazerlab.net" target="_blnak">David Lazer</a>.
+          I obtained my Ph.D. in Informatics from <a class="link" href="https://luddy.indiana.edu/" target="_blank">
+              Luddy School of Informatics, Computing, and Engineering</a>
+          in Indiana University Bloomington, advised by <a class="link" href="http://cnets.indiana.edu/fil/" target="_blank">Filippo Menczer</a>.
+        </p>
+        <p>
+          My research focuses on creating safe, fair, and trustworthy online information platforms by identifying how malicious actors and flawed systems distort information flow and developing effective countermeasures.
+          My work spans social bots, misinformation, and algorithmic biases.
+          Currently, I'm exploring how generative AI is being misused in these contexts and how to harness this technology to protect against these threats.
+        </p>
+        <p>
+          You can check out my <a class="link" href="#pubs">publications</a> to learn more.
+          These works have been <a class="link" href="#media">covered</a> by various media outlets.
+          You might also find some of my <a class="link" href="#tools">tools</a> useful.
+        </p>
       </div>
     </div>
-    <!-- bio -->
-    <div id="bio" class="md:col-span-2 mt-2 mx-4 prose">
-      <p>
-        Hi! I'm Kai-Cheng Yang (pronounced KY-cheng YAHNG). You can also call me Kevin.
-        I'm a postdoctoral researcher at Northeastern University working with <a class="link" href="https://lazerlab.net" target="_blnak">David Lazer</a>.
-        I obtained my Ph.D. in Informatics from <a class="link" href="https://luddy.indiana.edu/" target="_blank">
-            Luddy School of Informatics, Computing, and Engineering</a>
-        in Indiana University Bloomington, advised by <a class="link" href="http://cnets.indiana.edu/fil/" target="_blank">Filippo Menczer</a>.
-      </p>
-      <p>
-        My research focuses on creating safe, fair, and trustworthy online information platforms by identifying how malicious actors and flawed systems distort information flow and developing effective countermeasures.
-        My work spans social bots, misinformation, and algorithmic biases.
-        Currently, I'm exploring how generative AI is being misused in these contexts and how to harness this technology to protect against these threats.
-      </p>
-      <p>
-        You can check out my <a class="link" href="#pubs">publications</a> to learn more.
-        These works have been <a class="link" href="#media">covered</a> by various media outlets.
-        You might also find some of my <a class="link" href="#tools">tools</a> useful.
-      </p>
+    <div role="alert" class="alert alert-primary mt-4">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        class="stroke-primary h-6 w-6 shrink-0">
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
+      </svg>
+      <span class="text-primary">
+        I'm joining Binghamton University Computer Science as an Assistant Professor this Fall.
+        I'll recruit 2 motivated Ph.D. students. If you are interested in joining me, please send me an email with your CV and a short statement of purpose.
+      </span>
     </div>
   </div>
 </template>
