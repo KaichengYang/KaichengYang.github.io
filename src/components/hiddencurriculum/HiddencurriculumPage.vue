37a1a63b298fc049490a33649169e87d8d596f36
Kaicheng Yang <yang3kc@gmail.com>
2025-05-15 21:23:32 -0400

    ✨ feat(hiddencurriculum): create HiddencurriculumPage component with navbar and footer

---
+++ src/components/hiddencurriculum/HiddencurriculumPage.vue
@@ -0,0 +1,14 @@
+<script setup>
+  import Navbar from '@/components/nav/Navbar.vue'
+  import Footer from '@/components/nav/Footer.vue'
+  import HiddencurriculumList from '@/components/hiddencurriculum/HiddencurriculumList.vue'
+</script>
+
+<template>
+  <Navbar />
+  <div class="container mx-auto max-w-screen-lg">
+    <h1 class="text-4xl font-bold my-4 text-center">Hidden Curriculum</h1>
+    <HiddencurriculumList />
+  </div>
+  <Footer />
+</template>
