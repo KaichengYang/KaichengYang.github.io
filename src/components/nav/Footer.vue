f9f0ea7b057c5e4fa4acf05736002235d984137f
Kaicheng Yang <yang3kc@gmail.com>
2026-04-14 22:16:49 -0400

    chore: update footer copyright year to 2026

--- src/components/nav/Footer.vue
+++ src/components/nav/Footer.vue
@@ -2,7 +2,7 @@
   <footer class="footer footer-center bg-primary text-primary-content p-4 mt-4">
   <aside>
     <router-link to="/about">
-      <p>© 2024 - Kai-Cheng Yang | About</p>
+      <p>© 2026 - Kai-Cheng Yang | About</p>
     </router-link>
   </aside>
 </footer>
