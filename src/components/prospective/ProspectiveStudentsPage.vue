628956c88ae026dd4e23a6e519b558304d672f51
Kaicheng Yang <yang3kc@gmail.com>
2026-06-01 11:25:08 -0400

    prospective: update PhD openings text and reformat

--- src/components/prospective/ProspectiveStudentsPage.vue
+++ src/components/prospective/ProspectiveStudentsPage.vue
@@ -1,95 +1,139 @@
 <script setup>
-import Navbar from '@/components/nav/Navbar.vue'
-import Footer from '@/components/nav/Footer.vue'
+import Navbar from "@/components/nav/Navbar.vue";
+import Footer from "@/components/nav/Footer.vue";
 </script>
 
 <template>
-  <div class="flex flex-col flex-grow">
-    <Navbar />
-    <div class="container mx-auto max-w-screen-lg flex-grow">
+    <div class="flex flex-col flex-grow">
+        <Navbar />
+        <div class="container mx-auto max-w-screen-lg flex-grow">
+            <h1 class="text-4xl font-bold my-8 text-center">Join Yang Lab</h1>
 
-      <h1 class="text-4xl font-bold my-8 text-center">Join Yang Lab</h1>
+            <div class="max-w-4xl mx-auto">
+                <!-- Introduction -->
+                <div class="prose prose-lg mx-auto mb-8">
+                    <p class="text-xl"></p>
+                    <p class="text-xl">
+                        Interested in joining our lab? You can check out our
+                        <router-link to="/team" class="link">team</router-link>
+                        and
+                        <router-link to="/pubs" class="link"
+                            >publications</router-link
+                        >
+                        to learn more about the lab and our research.
+                    </p>
+                </div>
 
-      <div class="max-w-4xl mx-auto">
+                <!-- PhD Candidates -->
+                <div class="mb-12">
+                    <h2
+                        class="text-3xl font-bold mb-6 text-center text-primary"
+                    >
+                        Ph.D. Students
+                    </h2>
+                    <div class="prose prose-lg mx-auto">
+                        <h3>Openings</h3>
+                        <p>We don't currently have paid positions.</p>
+                        <h3>Preferred Background</h3>
+                        <p>
+                            The lab is open to students with a wide range of
+                            backgrounds. However, the admission committee will
+                            give more weight to students with backgrounds in
+                            computer science or related fields since the Ph.D.
+                            program is located in a Computer Science department.
+                            The admission committee only admits students with
+                            Master's degrees in principle, but undergraduates
+                            with exceptional backgrounds will also be
+                            considered.
+                        </p>
 
-        <!-- Introduction -->
-        <div class="prose prose-lg mx-auto mb-8">
-          <p class="text-xl">
-          </p>
-          <p class="text-xl">
-            Interested in joining our lab?
-            You can check out our <router-link to="/team" class="link">team</router-link> and <router-link to="/pubs" class="link">publications</router-link> to learn more about the lab and our research.
-          </p>
+                        <h3>Application Process</h3>
+                        <p>
+                            Please refer to the
+                            <a
+                                href="https://www.binghamton.edu/computer-science/graduate-programs"
+                                target="_blank"
+                                class="link"
+                                >Ph.D. program website</a
+                            >
+                            for instructions on how to apply. Prospective Ph.D.
+                            students are encouraged to reach out before
+                            submitting an application by emailing their research
+                            statements and CVs to
+                            <a href="mailto:yang3kc@gmail.com" class="link"
+                                >Kai-Cheng</a
+                            >
+                            (see communication section below for more details).
+                            Be sure to mention Kai-Cheng's name in submitted
+                            materials so that he can be notified of your
+                            application.
+                        </p>
+                    </div>
+                </div>
+
+                <!-- Other students & collaborators -->
+                <div class="mb-12">
+                    <h2
+                        class="text-3xl font-bold mb-6 text-center text-primary"
+                    >
+                        Other students & collaborators
+                    </h2>
+                    <div class="prose prose-lg mx-auto">
+                        <p>
+                            We are open to collaboration with students at
+                            Binghamton University and other institutions.
+                        </p>
+                        <h3 class="mt-2">Openings</h3>
+                        <p>
+                            Currently, we don't have the capacity to work with
+                            new students. Please check again in Fall 2026.
+                        </p>
+                        <h3 class="mt-2">Important Notes</h3>
+                        <p>
+                            Paid positions are unavailable unless stated
+                            otherwise, but we can provide research opportunities
+                            and resources for students who are interested in our
+                            work. Those who are interested in joining our lab
+                            should send
+                            <a href="mailto:yang3kc@gmail.com" class="link"
+                                >Kai-Cheng</a
+                            >
+                            an email with a brief statement of research
+                            interests, a CV, and/or a research proposal.
+                        </p>
+                    </div>
+                </div>
+
+                <!-- Communication -->
+                <div class="mb-12">
+                    <h2
+                        class="text-3xl font-bold mb-6 text-center text-primary"
+                    >
+                        Communication
+                    </h2>
+                    <div class="prose prose-lg mx-auto">
+                        <p>
+                            When emailing us, please include "[Prospective
+                            student]" (with brackets) in the subject line to
+                            reduce the chance of your email being overlooked.
+                        </p>
+                        <p>
+                            Due to the high volume of inquiries we receive, we
+                            cannot guarantee a timely response to all emails.
+                            However, we make every effort to respond to every
+                            genuine inquiry, except for:
+                        </p>
+                        <ul>
+                            <li>Emails addressed to the wrong person</li>
+                            <li>
+                                Generic emails that are not tailored to our
+                                research or lack important details
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
         </div>
-
-
-        <!-- PhD Candidates -->
-        <div class="mb-12">
-          <h2 class="text-3xl font-bold mb-6 text-center text-primary">Ph.D. Students</h2>
-          <div class="prose prose-lg mx-auto">
-            <h3>Openings</h3>
-            <p>
-              We are recruiting 1 motivated Ph.D. student for Fall 2026.
-            </p>
-            <h3>Preferred Background</h3>
-            <p>
-              The lab is open to students with a wide range of backgrounds.
-              However, the admission committee will give more weight to students with backgrounds in computer science or related fields since the Ph.D. program is located in a Computer Science department.
-              The admission committee only admits students with Master's degrees in principle, but undergraduates with exceptional backgrounds will also be considered.
-            </p>
-
-            <h3>Application Process</h3>
-            <p>
-              Please refer to the <a href="https://www.binghamton.edu/computer-science/graduate-programs" target="_blank" class="link">Ph.D. program website</a> for instructions on how to apply.
-              Prospective Ph.D. students are encouraged to reach out before submitting an application by emailing their research statements and CVs to <a href="mailto:yang3kc@gmail.com" class="link">Kai-Cheng</a> (see communication section below for more details).
-              Be sure to mention Kai-Cheng's name in submitted materials so that he can be notified of your application.
-            </p>
-
-          </div>
-        </div>
-
-        <!-- Other students & collaborators -->
-        <div class="mb-12">
-          <h2 class="text-3xl font-bold mb-6 text-center text-primary">Other students & collaborators</h2>
-          <div class="prose prose-lg mx-auto">
-            <p>
-              We are open to collaboration with students at Binghamton University and other institutions.
-            </p>
-            <h3 class="mt-2">Openings</h3>
-            <p>
-              Currently, we don't have the capacity to work with new students.
-              Please check again in Spring 2026.
-            </p>
-            <h3 class="mt-2">Important Notes</h3>
-            <p>
-              Paid positions are unavailable unless stated otherwise, but we can provide research opportunities and resources for students who are interested in our work.
-              Those who are interested in joining our lab should send <a href="mailto:yang3kc@gmail.com" class="link">Kai-Cheng</a> an email with a brief statement of research interests, a CV, and/or a research proposal.
-            </p>
-          </div>
-        </div>
-
-        <!-- Communication -->
-        <div class="mb-12">
-          <h2 class="text-3xl font-bold mb-6 text-center text-primary">Communication</h2>
-          <div class="prose prose-lg mx-auto">
-            <p>
-              When emailing us, please include "[Prospective student]" (with brackets) in the subject line to reduce the chance of your email being overlooked.
-            </p>
-            <p>
-              Due to the high volume of inquiries we receive, we cannot guarantee a timely response to all emails.
-              However, we make every effort to respond to every genuine inquiry, except for:
-            </p>
-            <ul>
-              <li>Emails addressed to the wrong person</li>
-              <li>Generic emails that are not tailored to our research or lack important details</li>
-            </ul>
-          </div>
-        </div>
-
-      </div>
-
-
+        <Footer />
     </div>
-    <Footer />
-  </div>
 </template>
