3029a6ba08c35e776ffef5e484248040e3cbca77
Kaicheng Yang <yang3kc@gmail.com>
2026-06-02 16:27:46 -0400

    cleanup: remove unused Bio.vue component

--- src/components/lab/LabIntro.vue
+++ src/components/lab/LabIntro.vue
@@ -1,32 +1,56 @@
-<script setup>
-</script>
+<script setup></script>
 
 <template>
-  <div>
-    <div class="gap-4 mt-4">
-      <h1 class="text-4xl font-medium mb-4 flex justify-center">Welcome to Yang Lab @ Binghamton University</h1>
-      <!-- lab description -->
-      <div id="lab_description" class="mt-2 mx-4 prose">
-        <p>
-          We are a research group led by <router-link to="/kaicheng" class="link">Dr. Kai-Cheng Yang</router-link>, who is an Assistant Professor in the School of Computing at Binghamton University.
-        </p>
-        <p>
-          Our lab studies how generative AI is transforming online information ecosystems, combining computational methods, data science, and social science approaches.
-        </p>
-        <p>
-          Learn more about our research by exploring our <router-link to="/team" class="link">team</router-link> and <a class="link" href="#pubs">publications</a>.
-          Our work has been <a class="link" href="#media">featured</a> in various media outlets, and you may find our <a class="link" href="#tools">research tools</a> and <a class="link" href="#datasets">datasets & benchmarks</a> helpful for your own projects.
-          We've also compiled a <router-link to="/hiddencurriculum" class="link">collection of resources</router-link> that researchers might find useful.
-        </p>
-        <p>
-          For prospective students, please see <router-link to="/prospective" class="link">here</router-link> for more information about joining our lab.
-        </p>
-        <p>
-          For students seeking recommendation letters, please see <router-link to="/recommendation-letters" class="link">here</router-link> for more information.
-        </p>
-      </div>
+    <div>
+        <div class="gap-4 mt-4">
+            <h1 class="text-4xl font-medium mb-4 flex justify-center">
+                Welcome to Yang Lab @ Binghamton University
+            </h1>
+            <!-- lab description -->
+            <div id="lab_description" class="mt-2 mx-4 prose">
+                <p>
+                    We are a research group led by
+                    <router-link to="/kaicheng" class="link"
+                        >Dr. Kai-Cheng Yang</router-link
+                    >, who is an Assistant Professor in the School of Computing
+                    at Binghamton University.
+                </p>
+                <p>
+                    Our lab studies how generative AI is transforming online
+                    information ecosystems, combining computational methods,
+                    data science, and social science approaches.
+                </p>
+                <p>
+                    Learn more about our research by exploring our
+                    <router-link to="/team" class="link">team</router-link> and
+                    <a class="link" href="#pubs">publications</a>. Our work has
+                    been <a class="link" href="#media">featured</a> in various
+                    media outlets, and you may find our
+                    <a class="link" href="#tools">research tools</a> and
+                    <a class="link" href="#datasets">datasets & benchmarks</a>
+                    helpful for your own projects. We've also compiled a
+                    <router-link to="/hiddencurriculum" class="link"
+                        >collection of resources</router-link
+                    >
+                    that researchers might find useful.
+                </p>
+                <p>
+                    For prospective students, please see
+                    <router-link to="/prospective" class="link"
+                        >here</router-link
+                    >
+                    for more information about joining our lab.
+                </p>
+                <p>
+                    For students seeking recommendation letters, please see
+                    <router-link to="/recommendation-letters" class="link"
+                        >here</router-link
+                    >
+                    for more information.
+                </p>
+            </div>
+        </div>
     </div>
-  </div>
 </template>
 
 <style scoped>
